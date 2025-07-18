e737b280e034cd05c19a527be412934f88b49ab4
Den Max <demax333@gmail.com>
2025-07-18 21:56:51 +0300

    Initial public data

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function (event) {
+  // Perform any installation steps here
+})
+
+self.addEventListener('activate', function (event) {
+  // Perform any activation steps here
+})
